b532482c4687aed787633e2b158c15fdbc957253
Scott Davidson <scottdavidson1992@gmail.com>
2024-02-25 09:54:08 +0000

    First somewhat complete keymap for stdavids

--- keyboards/gmmk/pro/rev1/ansi/keymaps/stdavids/config.h
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/stdavids/config.h
@@ -2,54 +2,125 @@
 
 #ifdef RGB_MATRIX_ENABLE
 
-    // USE THIS TO SET A DEFAULT RGB MODE
-    #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+    #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_GRADIENT_UP_DOWN     // Sets the default mode, if none has been set
+    #define RGB_MATRIX_DEFAULT_ON   true                            // Sets the default enabled state, if none has been set
+    #define RGB_MATRIX_TIMEOUT      0                               // Number of milliseconds to wait until rgb automatically turns off
+                                                                    //
+    #define RGB_MATRIX_DEFAULT_VAL  RGB_MATRIX_MAXIMUM_BRIGHTNESS   // Sets the default brightness value, if none has been set
+    #define RGB_MATRIX_DEFAULT_HUE  96                              // Sets the default hue value, if none has been set
+    #define RGB_MATRIX_DEFAULT_SAT  192                             // Sets the default saturation value, if none has been set
+    #define RGB_MATRIX_DEFAULT_SPD  96                              // Sets the default animation speed, if none has been set
 
-    // COMMENT THESE TO ENABLE (CHECK THAT THESE ARE ALL OF THEM)
+    ////////////////////////////////////////////////////////////
+    //              INDEX VALUES FOR PER-KEY RGB
+    ////////////////////////////////////////////////////////////
 
-    //#undef ENABLE_RGB_MATRIX_SOLID_COLOR	// it appears that you actually cannot disable this one...
-    #undef ENABLE_RGB_MATRIX_ALPHAS_MODS
-    #undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-    #undef ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
-    #undef ENABLE_RGB_MATRIX_BREATHING
-    #undef ENABLE_RGB_MATRIX_BAND_SAT
-    #undef ENABLE_RGB_MATRIX_BAND_VAL
-    #undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-    #undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
-    #undef ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
-    #undef ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
-    #undef ENABLE_RGB_MATRIX_CYCLE_ALL
-    #undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-    #undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
-    #undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-    #undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
-    #undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
-    #undef ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
-    #undef ENABLE_RGB_MATRIX_CYCLE_SPIRAL
-    #undef ENABLE_RGB_MATRIX_DUAL_BEACON
-    #undef ENABLE_RGB_MATRIX_RAINBOW_BEACON
-    #undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
-    #undef ENABLE_RGB_MATRIX_RAINDROPS
-    #undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-    #undef ENABLE_RGB_MATRIX_HUE_BREATHING
-    #undef ENABLE_RGB_MATRIX_HUE_PENDULUM
-    #undef ENABLE_RGB_MATRIX_HUE_WAVE
-    #undef ENABLE_RGB_MATRIX_PIXEL_RAIN
-    #undef ENABLE_RGB_MATRIX_PIXEL_FLOW
-    #undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL
-    #undef ENABLE_RGB_MATRIX_TYPING_HEATMAP
-    #undef ENABLE_RGB_MATRIX_DIGITAL_RAIN
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-    #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-    #undef ENABLE_RGB_MATRIX_SPLASH
-    #undef ENABLE_RGB_MATRIX_MULTISPLASH
-    #undef ENABLE_RGB_MATRIX_SOLID_SPLASH
-    #undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+    #define RGB_IDX_ESC     0
+    #define RGB_IDX_F1      6
+    #define RGB_IDX_F2      12
+    #define RGB_IDX_F3      18
+    #define RGB_IDX_F4      23
+    #define RGB_IDX_F5      28
+    #define RGB_IDX_F6      34
+    #define RGB_IDX_F7      39
+    #define RGB_IDX_F8      44
+    #define RGB_IDX_F9      50
+    #define RGB_IDX_F10     56
+    #define RGB_IDX_F11     61
+    #define RGB_IDX_F12     66
+    #define RGB_IDX_PRT     69
+
+    #define RGB_IDX_TICK    1
+    #define RGB_IDX_1       7
+    #define RGB_IDX_2       13
+    #define RGB_IDX_3       19
+    #define RGB_IDX_4       24
+    #define RGB_IDX_5       29
+    #define RGB_IDX_6       35
+    #define RGB_IDX_7       40
+    #define RGB_IDX_8       45
+    #define RGB_IDX_9       51
+    #define RGB_IDX_0       57
+    #define RGB_IDX_MINUS   62
+    #define RGB_IDX_EQUAL   78
+    #define RGB_IDX_BACKSPC 85
+
+    #define RGB_IDX_TAB     2
+    #define RGB_IDX_Q       8
+    #define RGB_IDX_W       14
+    #define RGB_IDX_E       20
+    #define RGB_IDX_R       25
+    #define RGB_IDX_T       30
+    #define RGB_IDX_Y       36
+    #define RGB_IDX_U       41
+    #define RGB_IDX_I       46
+    #define RGB_IDX_O       52
+    #define RGB_IDX_P       5
+    #define RGB_IDX_LBKT    63
+    #define RGB_IDX_RBKT    89
+    #define RGB_IDX_BSLASH  93
+
+    #define RGB_IDX_CAPS    3
+    #define RGB_IDX_A       9
+    #define RGB_IDX_S       15
+    #define RGB_IDX_D       21
+    #define RGB_IDX_F       26
+    #define RGB_IDX_G       31
+    #define RGB_IDX_H       37
+    #define RGB_IDX_J       42
+    #define RGB_IDX_K       47
+    #define RGB_IDX_L       53
+    #define RGB_IDX_SEMI    59
+    #define RGB_IDX_QUOTE   64
+    #define RGB_IDX_ENTER   96
+
+    #define RGB_IDX_LSHIFT  4
+    #define RGB_IDX_Z       10
+    #define RGB_IDX_X       16
+    #define RGB_IDX_C       22
+    #define RGB_IDX_V       27
+    #define RGB_IDX_B       32
+    #define RGB_IDX_N       38
+    #define RGB_IDX_M       43
+    #define RGB_IDX_COMMA   48
+    #define RGB_IDX_PERIOD  54
+    #define RGB_IDX_SLASH   60
+    #define RGB_IDX_RSHIFT  90
+
+    #define RGB_IDX_LCTRL   5
+    #define RGB_IDX_LWIN    11
+    #define RGB_IDX_LALT    17
+    #define RGB_IDX_SPACE   33
+    #define RGB_IDX_RALT    49
+    #define RGB_IDX_FN      55
+    #define RGB_IDX_RCTRL   65
+
+    #define RGB_IDX_UP      94
+    #define RGB_IDX_LEFT    95
+    #define RGB_IDX_DOWN    97
+    #define RGB_IDX_RIGHT   79
+
+    #define RGB_IDX_DEL     72
+    #define RGB_IDX_PGUP    75
+    #define RGB_IDX_PGDN    86
+    #define RGB_IDX_END     82
+
+    #define RGB_IDX_LSIDE_0 67
+    #define RGB_IDX_LSIDE_1 70
+    #define RGB_IDX_LSIDE_2 73
+    #define RGB_IDX_LSIDE_3 76
+    #define RGB_IDX_LSIDE_4 80
+    #define RGB_IDX_LSIDE_5 83
+    #define RGB_IDX_LSIDE_6 87
+    #define RGB_IDX_LSIDE_7 91
+
+    #define RGB_IDX_RSIDE_0 68
+    #define RGB_IDX_RSIDE_1 71
+    #define RGB_IDX_RSIDE_2 74
+    #define RGB_IDX_RSIDE_3 77
+    #define RGB_IDX_RSIDE_4 81
+    #define RGB_IDX_RSIDE_5 84
+    #define RGB_IDX_RSIDE_6 88
+    #define RGB_IDX_RSIDE_7 92
 
 #endif // RGB_MATRIX_ENABLE
--- keyboards/gmmk/pro/rev1/ansi/keymaps/stdavids/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/stdavids/keymap.c
@@ -1,6 +1,44 @@
 #include QMK_KEYBOARD_H
 
+bool g_dbg_state = false;
+
+extern void rgb_matrix_update_pwm_buffers(void);
+
+
+// This code should only work on devices that use the aw20216s LED driver. This code is based on the
+// rgb_matrix_set_color function found in qmk_firmware/drivers/led/aw20216s.c.
+uint8_t g_pwm_buffer[AW20216S_DRIVER_COUNT][216]; // AW20216S_PWM_REGISTER_COUNT = 216
+RGB rgb_matrix_get_color(int index)
+{
+    RGB rgb;
+    aw20216s_led_t led;
+    memcpy_P(&led, (&g_aw20216s_leds[index]), sizeof(led));
+    rgb.r = g_pwm_buffer[led.driver][led.r];
+    rgb.g = g_pwm_buffer[led.driver][led.g];
+    rgb.b = g_pwm_buffer[led.driver][led.b];
+    return rgb;
+}
+
+
+void rgb_matrix_invert_color(int index)
+{
+    RGB current, inverse;
+    current = rgb_matrix_get_color(index);
+    inverse.r = UINT8_MAX - current.r;
+    inverse.g = UINT8_MAX - current.g;
+    inverse.b = UINT8_MAX - current.b;
+    rgb_matrix_set_color(index, inverse.r, inverse.g, inverse.b);
+}
+
+
+enum custom_keycodes {
+    STD_DBG = SAFE_RANGE,
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    // BASE LAYER
     [0] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
@@ -9,21 +47,148 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
+
+    // FUNCTION LAYER
     [1] = LAYOUT(
-        _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          _______,
-        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          _______,
-        _______, _______, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,           EE_CLR,          _______,
-        _______,          _______, RGB_HUI, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  EE_CLR,          XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,          XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, STD_DBG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,          XXXXXXX,
+        XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NK_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,                            XXXXXXX, _______,   MO(2), XXXXXXX, XXXXXXX, XXXXXXX
     ),
+
+    // RGB LAYER
+    [2] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          RGB_TOG,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        XXXXXXX, RGB_SPD, RGB_SAI, RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        XXXXXXX, RGB_HUD, RGB_SAD, RGB_HUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,          XXXXXXX,
+        XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,                            XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+
 };
 
-#if defined(ENCODER_MAP_ENABLE)
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
+    [0] = { ENCODER_CCW_CW( KC_VOLD, KC_VOLU ) }, // BASE LAYER
+    [1] = { ENCODER_CCW_CW( XXXXXXX, XXXXXXX ) }, // FUNCTION LAYER
+    [2] = { ENCODER_CCW_CW( RGB_VAD, RGB_VAI ) }  // RGB LAYER
 };
 
-#endif // defined(ENCODER_MAP_ENABLE)
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+    switch (keycode) {
+        case STD_DBG:
+            if (record->event.pressed) {
+                // on pressed
+                g_dbg_state = !g_dbg_state;
+            } else {
+                // on released
+            }
+            break;
+    }
+    return true;
+}
+
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max)
+{
+    uint8_t layer = get_highest_layer(layer_state);
+
+    if (g_dbg_state) {
+        rgb_matrix_set_color(RGB_IDX_LSIDE_0, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_1, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_2, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_3, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_4, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_5, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_6, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_7, RGB_YELLOW);
+
+        rgb_matrix_set_color(RGB_IDX_RSIDE_0, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_1, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_2, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_3, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_4, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_5, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_6, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_7, RGB_YELLOW);
+
+        rgb_matrix_set_color(RGB_IDX_F5,  255,0,255);
+        rgb_matrix_set_color(RGB_IDX_F6,  255,0,255);
+        rgb_matrix_set_color(RGB_IDX_F7,  255,0,255);
+        rgb_matrix_set_color(RGB_IDX_F8,  255,0,255);
+        rgb_matrix_set_color(RGB_IDX_F9,  255,0,255);
+        rgb_matrix_set_color(RGB_IDX_F10, 255,0,255);
+        rgb_matrix_set_color(RGB_IDX_F11, 255,0,255);
+        rgb_matrix_set_color(RGB_IDX_F12, 255,0,255);
+
+        uint8_t val = 0;
+        if (layer == 0) {
+            val = rgb_matrix_get_hue();
+        } else if (layer == 1) {
+            val = rgb_matrix_get_sat();
+        } else if (layer == 2) {
+            val = rgb_matrix_get_speed();
+        }
+
+        if ((val >> 0) & 0x1) { rgb_matrix_invert_color(RGB_IDX_F5); }
+        if ((val >> 1) & 0x1) { rgb_matrix_invert_color(RGB_IDX_F6); }
+        if ((val >> 2) & 0x1) { rgb_matrix_invert_color(RGB_IDX_F7); }
+        if ((val >> 3) & 0x1) { rgb_matrix_invert_color(RGB_IDX_F8); }
+        if ((val >> 4) & 0x1) { rgb_matrix_invert_color(RGB_IDX_F9); }
+        if ((val >> 5) & 0x1) { rgb_matrix_invert_color(RGB_IDX_F10); }
+        if ((val >> 6) & 0x1) { rgb_matrix_invert_color(RGB_IDX_F11); }
+        if ((val >> 7) & 0x1) { rgb_matrix_invert_color(RGB_IDX_F12); }
+    }
+
+    // Invert the capslock key when capslock is enabled
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_invert_color(RGB_IDX_CAPS);
+    }
+
+    // When off of the base layer, highlight all bound keys
+    if (layer > 0) {
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+        for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+            uint8_t index = g_led_config.matrix_co[row][col];
+            if (index >= led_min && index < led_max && index != NO_LED) {
+                if (keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                    rgb_matrix_invert_color(index);
+                }
+            }
+        }}
+    }
+
+    return false;
+}
+
+
+bool shutdown_user(bool jump_to_bootloader)
+{
+    rgb_matrix_set_color_all(RGB_OFF);
+    if (jump_to_bootloader) {
+        rgb_matrix_set_color(RGB_IDX_LSIDE_0, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_1, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_2, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_3, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_4, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_5, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_6, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_LSIDE_7, RGB_YELLOW);
+
+        rgb_matrix_set_color(RGB_IDX_RSIDE_0, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_1, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_2, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_3, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_4, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_5, RGB_RED);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_6, RGB_YELLOW);
+        rgb_matrix_set_color(RGB_IDX_RSIDE_7, RGB_YELLOW);
+    }
+    rgb_matrix_update_pwm_buffers();
+    return false;
+}
